94f9f86c6594f3e65763fcabb194fe2e3855772a
SyedSohailAli1 <syedsohail4455@gmail.com>
2023-07-10 07:06:50 +0530

    Added Reward Value class and pass tests

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -24,36 +24,3 @@ public class RewardValue {
         return this.milesValue;
     }
 }
-
-
-
-
-
-
-
-
-
-
-//
-//public class RewardValue {
-//    private double cashValue;
-//    private double milesValue;
-//
-//    public RewardValue(double cashValue) {
-//        this.cashValue = cashValue;
-//        this.milesValue = cashValue / 0.0035;
-//    }
-//
-//    public RewardValue(double cashValue, double milesValue) {
-//        this.milesValue = milesValue;
-//        this.cashValue = milesValue * 0.0035;
-//    }
-//
-//    public double getCashValue() {
-//        return cashValue;
-//    }
-//
-//    public double getMilesValue() {
-//        return milesValue;
-//    }
-//}
